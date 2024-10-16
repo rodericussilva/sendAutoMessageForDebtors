69babed6f6c73ab7e8810a1e7bd318a0a37e2e07
Rodrigo Silva <112216918+rodericussilva@users.noreply.github.com>
2024-10-16 13:30:28 -0300

    Update rescheduling.py

--- rescheduling.py
+++ rescheduling.py
@@ -5,9 +5,6 @@ RESCHEDULING_FILE = 'rescheduling.json'
 LAST_SENT_FILE = 'last_sent_dates.json'
 
 def load_rescheduling():
-    """
-    Carrega os reagendamentos existentes de um arquivo JSON.
-    """
     try:
         with open(RESCHEDULING_FILE, 'r') as file:
             data = json.load(file)
@@ -16,18 +13,10 @@ def load_rescheduling():
         return []
 
 def save_rescheduling(rescheduling):
-    """
-    Salva os reagendamentos no arquivo JSON.
-    """
     with open(RESCHEDULING_FILE, 'w') as file:
         json.dump({"reagendamentos": rescheduling}, file, indent=4)
 
 def check_reschedule(name=None, number=None):
-    """
-    Verifica se há reagendado e se deve ser ignorado até nova data.
-    
-    :return: True se o cliente estiver reagendado, False caso contrário
-    """
     rescheduling = load_rescheduling()
     for reagendamento in rescheduling:
         if (name and reagendamento.get('name') == name) or (number and reagendamento.get('number') == number):
@@ -37,9 +26,6 @@ def check_reschedule(name=None, number=None):
     return False
 
 def load_last_sent_dates():
-    """
-    Carrega as últimas datas de envio de mensagens de um arquivo JSON.
-    """
     try:
         with open(LAST_SENT_FILE, 'r') as file:
             data = json.load(file)
@@ -48,16 +34,10 @@ def load_last_sent_dates():
         return []
 
 def save_last_sent_dates(last_sent_data):
-    """
-    Salva as últimas datas de envio de mensagens no arquivo JSON.
-    """
     with open(LAST_SENT_FILE, 'w') as file:
         json.dump({"clientes": last_sent_data}, file, indent=4)
 
 def get_last_sent_date(name, number):
-    """
-    Retorna a última data de envio para um cliente com base no nome e número.
-    """
     last_sent_data = load_last_sent_dates()
     for entry in last_sent_data:
         if entry['name'] == name and entry['number'] == number:
@@ -65,20 +45,15 @@ def get_last_sent_date(name, number):
     return None
 
 def update_last_sent_date(name, number):
-    """
-    Atualiza a última data de envio para um cliente.
-    """
     last_sent_data = load_last_sent_dates()
     updated = False
 
-    # Atualiza a data se o cliente já existe
     for entry in last_sent_data:
         if entry['name'] == name and entry['number'] == number:
             entry['last_sent'] = datetime.now().strftime("%Y-%m-%d")
             updated = True
             break
 
-    # Adiciona um novo cliente se não existir
     if not updated:
         last_sent_data.append({
             'name': name,
