3e11a288abb707804e84dd80703b842e2ea28e9f
Rodrigo Silva <112216918+rodericussilva@users.noreply.github.com>
2024-10-16 13:29:30 -0300

    Update notifications.py

--- notifications.py
+++ notifications.py
@@ -7,7 +7,6 @@ def record_success(id, name, number):
     #print(f"Mensagem enviada com sucesso para {name} ({number}).")
 
 def record_failure(id, name, number, error):
-    # :param erro: Descrição do erro ocorrido
 
     with open('log_failure.txt', 'a') as file:
         file.write(f"{datetime.now()} - Falha ao enviar mensagem para {name} ({number}): {error}\n")
