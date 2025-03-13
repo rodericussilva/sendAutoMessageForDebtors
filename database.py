3ba46a103d82b9e13e3bb575c7138ecd3e8e023e
Rodrigo Silva <112216918+rodericussilva@users.noreply.github.com>
2025-03-13 16:56:16 -0300

    Changing the query
    removing the view for direct query

--- database.py
+++ database.py
@@ -18,53 +18,99 @@ def search_debtors():
     conn = connect_db()
     cursor = conn.cursor()
     query = """
-        SELECT c.id, c.nome, c.telefone, b.dias_atrasados, c.email
-        FROM clientes c
-        JOIN boletos b ON c.id = b.id_cliente
-        WHERE b.status = 'vencido'
+        SELECT
+	        cl.Codigo,
+	        cl.Razao_Social,
+	        cl.Fone1,
+	        cl.Email,
+	        ct.Status,
+	        ct.Dat_Emissao,
+	        ct.Dat_Vencimento,
+	        ct.Vlr_Documento
+        FROM CLIEN AS cl
+        JOIN CTREC AS ct
+	        ON cl.Codigo = ct.Cod_Cliente
+		JOIN ENXES AS en 
+            ON ct.Cod_Cliente = en.Cod_Client and cod_regtri = '23' and cod_agente = '4'
+        WHERE ct.Status = 'A'
+        AND ct.Dat_Vencimento < ?
+        AND DATEDIFF(DAY, ct.Dat_Vencimento, GETDATE()) > 3
+        AND DATEDIFF(DAY, ct.Dat_Vencimento, GETDATE()) <= 20
+        GROUP BY
+	        cl.Codigo,
+	        cl.Razao_Social,
+	        cl.Fone1,
+	        cl.Email,
+	        ct.Status,
+	        ct.Dat_Emissao,
+	        ct.Dat_Vencimento,
+	        ct.Vlr_Documento;
     """
-    cursor.execute(query)
+    today = datetime.today()
+    cursor.execute(query, today)
     results = cursor.fetchall()
     conn.close()
 
+    customers_dict = {}
+
+    for row in results:
+        name = row.Razao_Social
+        number = row.Fone1
+        days_late = (today - row.Dat_Vencimento).days
+        email = row.Email
+        document_value = row.Vlr_Documento
+
+        if (name, number) not in customers_dict:
+            customers_dict[(name, number)] = {
+                'name': name,
+                'number': number,
+                'email': email,
+                'boletos': 0,
+                'total_days_late': 0,
+                'value': 0,
+            }
+
+        customers_dict[(name, number)]['boletos'] += 1
+
+        if days_late > customers_dict[(name, number)]['total_days_late']:
+            customers_dict[(name, number)]['total_days_late'] = days_late
+            customers_dict[(name, number)]['value'] = document_value
+
     customers = [
         {
-            'id': row.id,
-            'name': row.nome,
-            'number': row.telefone,
-            'days_late': row.dias_atrasados,
-            'email': row.email
+            'name': customer['name'],
+            'number': customer['number'],
+            'email': customer['email'],
+            'boletos': customer['boletos'],
+            'total_days_late': customer['total_days_late'],
+            'value': customer['value']
         }
-        for row in results
+        for customer in customers_dict.values()
     ]
-    return customers
 
+    return customers
+    
 def check_payment_status(name_customer=None, number_customer=None):
+    """
+    Verifica no banco de dados se o status do cliente foi quitado.
+    :return: True se o boleto foi pago (status Q), False caso contrário
+    """
+    if name_customer is None or number_customer is None:
+        return False
+
     conn = connect_db()
     cursor = conn.cursor()
 
     query = """
-        SELECT b.status
-        FROM clientes c
-        JOIN boletos b ON c.id = b.id_cliente
-        WHERE b.status = 'pago'
+        SELECT STATUS
+        FROM vw_client
+        WHERE RAZAO_SOCIAL = ? AND FONE1 = ?
     """
     
-    conditions = []
-    if name_customer:
-        conditions.append("c.nome = ?")
-    if number_customer:
-        conditions.append("c.telefone = ?")
-    
-    if conditions:
-        query += " AND (" + " OR ".join(conditions) + ")"
-
-    parameters = [param for param in (name_customer, number_customer) if param]
-    cursor.execute(query, parameters)
-    
+    cursor.execute(query, (name_customer, number_customer))
     result = cursor.fetchone()
     conn.close()
 
-    if result and result[0] == 'pago':
+    if result and result[0] == 'Q':
         return True
     return False
