4b9b423069d6662b0b35023a62a9f906a7fef8da
Rodrigo Silva <112216918+rodericussilva@users.noreply.github.com>
2025-03-14 15:30:34 -0300

    creation of new features
    functions for writing successful blogs and failures in sending emails in txt documents

--- notifications.py
+++ notifications.py
@@ -4,10 +4,18 @@ def record_success(id, name, number):
 
     with open('log_success.txt', 'a') as file:
         file.write(f"{datetime.now()} - Mensagem enviada com sucesso para {name} ({number}).\n")
-    #print(f"Mensagem enviada com sucesso para {name} ({number}).")
 
 def record_failure(id, name, number, error):
 
     with open('log_failure.txt', 'a') as file:
         file.write(f"{datetime.now()} - Falha ao enviar mensagem para {name} ({number}): {error}\n")
-    #print(f"Falha ao enviar mensagem para {name} ({number}): {error}")
+
+def record_email_success(recipient_email):
+    
+    with open('log_email_success.txt', 'a') as file:
+        file.write(f"{datetime.now()} - Email enviado com sucesso para {recipient_email}\n")
+        
+def record_email_failure(email, error_message):
+    
+    with open('log_email_failure.txt', 'a') as file:
+        file.write(f"Falha ao enviar email para {email}: {error_message}\n")
